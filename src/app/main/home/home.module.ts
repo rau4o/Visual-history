4ea07d6e2527a64ed8faf11166d740f7389ae478
Rauan <rkoshkinbayev@gmail.com>
2022-09-27 21:50:59 +0300

    sponsor page

--- src/app/main/home/home.module.ts
+++ src/app/main/home/home.module.ts
@@ -3,9 +3,11 @@ import {CommonModule} from '@angular/common';
 import {HomeComponent} from './home.component';
 import {HomeRoutingModule} from './home-routing.module';
 import { HomePageComponent } from './home-page/home-page.component';
+import { SponsorComponent } from './sponsor/sponsor.component';
+import { InformationComponent } from './information/information.component';
 
 @NgModule({
-  declarations: [HomeComponent, HomePageComponent],
+  declarations: [HomeComponent, HomePageComponent, SponsorComponent, InformationComponent],
   imports: [
     CommonModule,
     HomeRoutingModule
