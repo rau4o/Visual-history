c768782c2d05a278aad65c8cee443370f05ffabf
Rauan <rkoshkinbayev@gmail.com>
2022-09-27 19:09:14 +0300

    home page

--- src/app/main/home/home.module.ts
+++ src/app/main/home/home.module.ts
@@ -1,9 +1,14 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HomeComponent} from './home.component';
+import {HomeRoutingModule} from './home-routing.module';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
-  declarations: [HomeComponent],
-  imports: [CommonModule],
+  declarations: [HomeComponent, HomePageComponent],
+  imports: [
+    CommonModule,
+    HomeRoutingModule
+  ],
 })
 export class HomeModule {}
